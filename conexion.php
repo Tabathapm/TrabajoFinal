9c8d2fe3f60700a5a678e12d71aebcd496ecb99d
julieta96 <julietaextra14@gmail.com>
2021-06-20 17:17:54 -0300

    se elimino

--- conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $usuario="root";
-    $password="";
-    $servidor="localhost";
-    $nombrebasedatos="transportesunlam";
-    $con= new mysqli($servidor,$usuario,$password,$nombrebasedatos)or die("error de conexion".mysql_error());
